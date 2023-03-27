7a118d42fa2801aae88ec194d838d963102c285e
Lili Mkrtchyan <lm688@cornell.edu>
2023-03-27 02:17:09 +0000

    Created pages

---
+++ pages/products.php
@@ -0,0 +1,22 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>Products</title>
+</head>
+
+<body>
+
+  <p>All the ArmJewelers products!</p>
+
+</body>
+
+</html>
